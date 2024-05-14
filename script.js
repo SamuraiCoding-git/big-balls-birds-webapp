0c7bdaa8db1aa046cd54b09ed207223fa999ae26
matveydoroshenko <103792284+matveydoroshenko@users.noreply.github.com>
2024-05-14 10:16:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var music = document.getElementById('background-music');
 
     // Example functionality to toggle play/pause for the video
-    video.addEventListener('click', function() {
-        if (video.paused) {
-            video.play();
-        } else {
-            video.pause();
-        }
-    });
 
     // Ensure the music plays as expected (some browsers may block autoplay)
     music.play().catch(error => {
